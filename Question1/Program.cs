38833b6d94cff5c141236b5264c3d9c9b3b8ac42
Nachiket Dabhade <dabhadenachiket@gmail.com>
2023-01-29 17:13:50 -0500

    Initial Commit
    Created the initial structure for GitHub

---
+++ Question1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace Question1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int N = r.Next(0, 100);
+            int[] A = new int[N];
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                A[i] = r.Next(-1000, 1000);
+            }
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                Console.WriteLine(A[i]);
+            }
+
+            Array.Sort(A);
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                Console.WriteLine(A[i]);
+            }
+
+            A.Where((a, index) => a >= 0);
+
+            for (int i = 0; i < A.Length; i++)
+            {
+                Console.WriteLine(A[i]);
+            }
+        }
+    }
+}
